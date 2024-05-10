11460b73c9d379386a3a214caf611d121dba3b04
unknown <Kuanysh>
2024-05-10 16:28:11 +0600

    new logic

--- db_module.py
+++ db_module.py
@@ -36,6 +36,23 @@ def get_pwd():
             cursor.close()
             conn.close()
 
+def db_pass(db_name):
+    conn = db_connect()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT actual_password FROM passwords_test_vault WHERE db_name = '{db_name}'")
+            data = cursor.fetchone()
+
+            if data:
+                return data[0]
+
+        except psycopg2.Error as e:
+            print(e)
+        finally:
+            cursor.close()
+            conn.close()
+
 
 print(get_pwd())
 
